3869d6af7b2a936ca7cdef03c3f812764d2e7db0
theburningmonk <theburningmonk@gmail.com>
2016-07-01 02:56:06 +0200

    - wip

--- snake.elm
+++ snake.elm
@@ -3,6 +3,12 @@ import Html.App as Html
 import Html.Events exposing (..)
 import Keyboard exposing (..)
 import Time exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+import Color exposing (..)
+import Text exposing (..)
+
+(width, height) = (500, 500)
 
 main =
   Html.program
@@ -24,6 +30,13 @@ type alias Snake =
   { head: Position
   , tail: List Position
   , direction: Direction 
+  }
+
+initSnake : Snake
+initSnake = 
+  { head : (0, 0)
+  , tail : [1..8] |> List
+
   }
 
 type Model 
@@ -54,8 +67,37 @@ subscriptions model =
 
 -- step 5: how to render your model
 view : Model -> Html Msg
-view model = div [] []
+view model =
+  let bg = rect (toFloat width) (toFloat height) |> filled black
+      content =
+        case model of
+          NotStarted -> 
+            [ txt "press SPACE to start" ]
+          
+          Started _ ->
+            []
+  in collage width height (bg::content)
+     |> Element.toHtml
+
 
 -- step 6: implement state transition
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = (NotStarted, Cmd.none)
+update msg model =
+  case model of
+    NotStarted ->
+      case msg of
+        KeyPress 32 -> 
+          (Started initSnake, Cmd.none)
+
+        _ -> (model, Cmd.none)
+
+
+
+txt : String -> Form
+txt msg =
+  msg
+  |> Text.fromString
+  |> Text.color white
+  |> Text.monospace
+  |> Element.centered
+  |> Collage.toForm
